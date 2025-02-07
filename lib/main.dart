02500bbfee4fe63a7d4321df3197c6e95cfa7263
Apurboraj <apucse1296@gmail.com>
2025-02-07 19:25:30 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -34,199 +34,258 @@ class Home extends StatelessWidget {
         ),),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-
-          Padding(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+        
+            Padding(
+              padding: const EdgeInsets.only(left: 20,right: 20,top: 20),
+              child: TextField(
+                controller: _numberController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    filled: false,
+                    fillColor: Colors.amberAccent,
+                    hintText: "Enter your Phone Number",
+                    hintStyle: TextStyle(fontSize: 15,color: Colors.black),
+                    hintMaxLines: 2,
+                    labelText: "Number",
+                    labelStyle: TextStyle(fontSize: 25,color: Colors.blue),
+                    helperText: "Please enter your phone Number",
+                    helperStyle: TextStyle(fontSize: 15,color: Colors.green),
+                    helperMaxLines: 1,
+                    //prefixText: "Email: ",
+                    //suffixText: "@gmail.com",
+                    prefixIcon: Icon(Icons.phone),
+                    suffixIcon: Icon(Icons.send),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.amber,width: 5),
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey,width: 2),
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
+                    )
+                ),
+              ),
+            ),
+        
+            Padding(
             padding: const EdgeInsets.only(left: 20,right: 20,top: 20),
             child: TextField(
-              controller: _numberController,
-              keyboardType: TextInputType.number,
+              controller: _emailController,
               decoration: InputDecoration(
-                  filled: false,
-                  fillColor: Colors.amberAccent,
-                  hintText: "Enter your Phone Number",
-                  hintStyle: TextStyle(fontSize: 15,color: Colors.black),
-                  hintMaxLines: 2,
-                  labelText: "Number",
-                  labelStyle: TextStyle(fontSize: 25,color: Colors.blue),
-                  helperText: "Please enter your phone Number",
-                  helperStyle: TextStyle(fontSize: 15,color: Colors.green),
-                  helperMaxLines: 1,
-                  //prefixText: "Email: ",
-                  //suffixText: "@gmail.com",
-                  prefixIcon: Icon(Icons.phone),
-                  suffixIcon: Icon(Icons.send),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.amber,width: 5),
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey,width: 2),
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
-                  )
+                filled: false,
+                fillColor: Colors.amberAccent,
+                hintText: "Enter your email",
+                hintStyle: TextStyle(fontSize: 15,color: Colors.black),
+                hintMaxLines: 2,
+                labelText: "Email",
+                labelStyle: TextStyle(fontSize: 25,color: Colors.blue),
+                helperText: "Please enter your email",
+                helperStyle: TextStyle(fontSize: 15,color: Colors.green),
+                helperMaxLines: 1,
+                //prefixText: "Email: ",
+                //suffixText: "@gmail.com",
+                prefixIcon: Icon(Icons.email),
+                suffixIcon: Icon(Icons.send),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey,width: 5),
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey,width: 2),
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
+                )
               ),
             ),
           ),
-
-          Padding(
-          padding: const EdgeInsets.only(left: 20,right: 20,top: 20),
-          child: TextField(
-            controller: _emailController,
-            decoration: InputDecoration(
-              filled: false,
-              fillColor: Colors.amberAccent,
-              hintText: "Enter your email",
-              hintStyle: TextStyle(fontSize: 15,color: Colors.black),
-              hintMaxLines: 2,
-              labelText: "Email",
-              labelStyle: TextStyle(fontSize: 25,color: Colors.blue),
-              helperText: "Please enter your email",
-              helperStyle: TextStyle(fontSize: 15,color: Colors.green),
-              helperMaxLines: 1,
-              //prefixText: "Email: ",
-              //suffixText: "@gmail.com",
-              prefixIcon: Icon(Icons.email),
-              suffixIcon: Icon(Icons.send),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey,width: 5),
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey,width: 2),
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
-              )
-            ),
-          ),
-        ),
-
-          Padding(
-            padding: const EdgeInsets.only(left: 20,right: 20,top: 20),
-            child: TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                  filled: false,
-                  fillColor: Colors.amberAccent,
-                  hintText: "Enter your Password",
-                  hintStyle: TextStyle(fontSize: 15,color: Colors.black),
-                  hintMaxLines: 2,
-                  labelText: "Password",
-                  labelStyle: TextStyle(fontSize: 25,color: Colors.blue),
-                  helperText: "Please enter your email",
-                  helperStyle: TextStyle(fontSize: 15,color: Colors.green),
-                  helperMaxLines: 1,
-                  //prefixText: "Email: ",
-                  //suffixText: "@gmail.com",
-                  prefixIcon: Icon(Icons.password),
-                  suffixIcon: Icon(Icons.send),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.amber,width: 5),
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey,width: 2),
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
-                  )
+        
+            Padding(
+              padding: const EdgeInsets.only(left: 20,right: 20,top: 20),
+              child: TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    filled: false,
+                    fillColor: Colors.amberAccent,
+                    hintText: "Enter your Password",
+                    hintStyle: TextStyle(fontSize: 15,color: Colors.black),
+                    hintMaxLines: 2,
+                    labelText: "Password",
+                    labelStyle: TextStyle(fontSize: 25,color: Colors.blue),
+                    helperText: "Please enter your email",
+                    helperStyle: TextStyle(fontSize: 15,color: Colors.green),
+                    helperMaxLines: 1,
+                    //prefixText: "Email: ",
+                    //suffixText: "@gmail.com",
+                    prefixIcon: Icon(Icons.password),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.amber,width: 5),
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey,width: 2),
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
+                    )
+                ),
               ),
             ),
-          ),
-ElevatedButton(onPressed: (){
-  _passwordController.text.length > 6 ? print("password is strong") : print("password is weak");
-  print(_numberController.text);
-  print(_emailController.text);
-  print(_passwordController.text);
-}, child: Text("Submit")),
-
-          ElevatedButton(onPressed: (){
-            _numberController.clear();
-            _emailController.clear();
-            _passwordController.clear();
-          }, child: Text("Clear")),
-
-///Flutter old class
-      //     Center(
-      //       child: ElevatedButton(
-      //           style: ElevatedButton.styleFrom(
-      //             backgroundColor: Colors.green,
-      //             shape: RoundedRectangleBorder(
-      //               borderRadius: BorderRadius.circular(10),
-      //             ),
-      //           ),
-      //           onPressed: (){
-      //             print("This is a button");
-      //           }, child: Text("Send",style: TextStyle(color: Colors.white,fontSize: 25),)),
-      //     ),
-      //     Icon(Icons.add_a_photo,size: 50, color: Colors.red,),
-      //     TextButton(onPressed: (){
-      //       print("I'm text button");
-      //     }, child: Text("Click me",style: TextStyle(color: Colors.blue,fontSize: 25),)),
-      //     IconButton(onPressed: (){
-      //       print("I'm a icon button");
-      //     }, icon: Icon(Icons.add_a_photo,size: 50, color: Colors.red,)),
-      //     InkWell(
-      //       onTap: (){
-      //         print("I'm inkwell");
-      //       },
-      //       onHover: (value){
-      //         print("I'm inkwell hover");
-      //       },
-      //       onLongPress: (){
-      //         print("I'm inkwell long press");
-      //       },
-      //       child: Container(
-      //         height: 200,
-      //         width: 200,
-      //         margin: EdgeInsets.only(top: 50),
-      //         alignment: Alignment.center,
-      //         decoration: BoxDecoration(
-      //           color: Colors.deepPurpleAccent,
-      //           borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
-      //           border: Border.all(color: Colors.black,width: 3),
-      //           gradient: LinearGradient(colors: [Colors.yellow.shade300,Colors.orange,Colors.red,Colors.black],
-      //             begin:Alignment.topLeft,
-      //             end:Alignment.bottomRight,
-      //           ),
-      //           boxShadow: [
-      //             BoxShadow(
-      //                 color: Colors.grey,
-      //                 blurRadius: 10,
-      //                 spreadRadius: 5,
-      //                 offset: Offset(5, 5)
-      //             )
-      //           ],
-      //         ),
-      //         child: Text("This is a container",style: TextStyle(color: Colors.blue,fontSize: 25),),
-      //       ),
-      //     ),
-      //     GestureDetector(
-      //       child: Container(
-      //         height: 200,
-      //         width: 200,
-      //         margin: EdgeInsets.only(top: 50),
-      //         alignment: Alignment.center,
-      //         decoration: BoxDecoration(
-      //           color: Colors.deepPurpleAccent,
-      //           borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
-      //           border: Border.all(color: Colors.black,width: 3),
-      //           gradient: LinearGradient(colors: [Colors.green,Colors.orange.withOpacity(0.5),Colors.black],
-      //             begin:Alignment.topLeft,
-      //             end:Alignment.bottomRight,
-      //           ),
-      //           boxShadow: [
-      //             BoxShadow(
-      //                 color: Colors.grey,
-      //                 blurRadius: 10,
-      //                 spreadRadius: 5,
-      //                 offset: Offset(0, 5)
-      //             )
-      //           ],
-      //         ),
-      //         child: Text("This is a GestureDetector",style: TextStyle(color: Colors.blue,fontSize: 25),),
-      //       ),
-      //     ),
-        ],
-       ),
+        
+            ElevatedButton(onPressed: (){
+          _passwordController.text.length > 6
+        ? print("password is strong")
+        : print("password is weak");
+          print(_numberController.text);
+          print('${_emailController.text}@gmail.com');
+          print(_passwordController.text);
+        }, child: Text("Submit")),
+        
+            ElevatedButton(onPressed: (){
+              _numberController.clear();
+              _emailController.clear();
+              _passwordController.clear();
+            }, child: Text("Clear")),
+        
+            Text("This is text 1", style: TextStyle(fontSize: 25, color: Colors.amber),),
+            Divider(),
+            Text("This is text 1", style: TextStyle(fontSize: 25, color: Colors.amber),),
+         ListTile(
+           title: Text("Apurbo RajBongshi"),
+           subtitle: Text("Software Engineer"),
+           leading: Icon(Icons.account_circle,size: 50,),
+           trailing: Icon(Icons.send),
+           onTap: (){
+             print("I'm LisrTile");
+           },
+           onLongPress: (){
+             print("I'm ListTile long press");
+           },
+         ),
+           SizedBox(
+             width: double.infinity,
+             child: ElevatedButton(
+                 onPressed: (){},
+                 child: ElevatedButton(
+                   onPressed: (){},
+                   child: Text(
+                     "Click me",
+                     style: TextStyle(fontSize: 25,color: Colors.white),
+            ),
+            style:
+            ElevatedButton.styleFrom(backgroundColor: Colors.green),
+            )),
+           ),
+        
+           SizedBox(
+             height: 250,
+             width: 250,
+             child: ListView.builder(
+               itemCount: 100,
+               itemBuilder: (context,index){
+                 return  ListTile(
+                   title: Text("Apurbo RajBongshi ${index}"),
+                   subtitle: Text("Software Engineer"),
+                   leading: Icon(Icons.account_circle,size: 50,),
+                   trailing: Icon(Icons.send),
+                   onTap: (){
+                     print("I'm LisrTile");
+                   },
+                   onLongPress: (){
+                     print("I'm ListTile long press");
+                   },
+                 );
+               },
+             ),
+           )
+        
+        
+        
+        ///Flutter old class
+        //     Center(
+        //       child: ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             backgroundColor: Colors.green,
+        //             shape: RoundedRectangleBorder(
+        //               borderRadius: BorderRadius.circular(10),
+        //             ),
+        //           ),
+        //           onPressed: (){
+        //             print("This is a button");
+        //           }, child: Text("Send",style: TextStyle(color: Colors.white,fontSize: 25),)),
+        //     ),
+        //     Icon(Icons.add_a_photo,size: 50, color: Colors.red,),
+        //     TextButton(onPressed: (){
+        //       print("I'm text button");
+        //     }, child: Text("Click me",style: TextStyle(color: Colors.blue,fontSize: 25),)),
+        //     IconButton(onPressed: (){
+        //       print("I'm a icon button");
+        //     }, icon: Icon(Icons.add_a_photo,size: 50, color: Colors.red,)),
+        //     InkWell(
+        //       onTap: (){
+        //         print("I'm inkwell");
+        //       },
+        //       onHover: (value){
+        //         print("I'm inkwell hover");
+        //       },
+        //       onLongPress: (){
+        //         print("I'm inkwell long press");
+        //       },
+        //       child: Container(
+        //         height: 200,
+        //         width: 200,
+        //         margin: EdgeInsets.only(top: 50),
+        //         alignment: Alignment.center,
+        //         decoration: BoxDecoration(
+        //           color: Colors.deepPurpleAccent,
+        //           borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+        //           border: Border.all(color: Colors.black,width: 3),
+        //           gradient: LinearGradient(colors: [Colors.yellow.shade300,Colors.orange,Colors.red,Colors.black],
+        //             begin:Alignment.topLeft,
+        //             end:Alignment.bottomRight,
+        //           ),
+        //           boxShadow: [
+        //             BoxShadow(
+        //                 color: Colors.grey,
+        //                 blurRadius: 10,
+        //                 spreadRadius: 5,
+        //                 offset: Offset(5, 5)
+        //             )
+        //           ],
+        //         ),
+        //         child: Text("This is a container",style: TextStyle(color: Colors.blue,fontSize: 25),),
+        //       ),
+        //     ),
+        //     GestureDetector(
+        //       child: Container(
+        //         height: 200,
+        //         width: 200,
+        //         margin: EdgeInsets.only(top: 50),
+        //         alignment: Alignment.center,
+        //         decoration: BoxDecoration(
+        //           color: Colors.deepPurpleAccent,
+        //           borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+        //           border: Border.all(color: Colors.black,width: 3),
+        //           gradient: LinearGradient(colors: [Colors.green,Colors.orange.withOpacity(0.5),Colors.black],
+        //             begin:Alignment.topLeft,
+        //             end:Alignment.bottomRight,
+        //           ),
+        //           boxShadow: [
+        //             BoxShadow(
+        //                 color: Colors.grey,
+        //                 blurRadius: 10,
+        //                 spreadRadius: 5,
+        //                 offset: Offset(0, 5)
+        //             )
+        //           ],
+        //         ),
+        //         child: Text("This is a GestureDetector",style: TextStyle(color: Colors.blue,fontSize: 25),),
+        //       ),
+        //     ),
+          ],
+         ),
+      ),
     );
   }
 }
