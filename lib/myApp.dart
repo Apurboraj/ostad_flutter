98c07ec6b09ca92c11f0262f5ec046e8ef393bf1
Apurboraj <apucse1296@gmail.com>
2025-03-03 22:01:54 +0600

    updated

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -1,29 +1,38 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 import 'Home.dart';
 import 'module_9_1.dart';
 import 'module_9_2.dart';
-
+import 'module_9_3.dart';
 class myApp extends StatelessWidget {
   const myApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: Colors.red,
-          appBarTheme: AppBarTheme(
-            color: Colors.red,
-            centerTitle: true,
-          ),
-          scaffoldBackgroundColor: Colors.white,
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.deepPurpleAccent,
-                  foregroundColor: Colors.white))),
-      title: 'Ostad Flutter app',
-      home: ModuleNine(),
+    return Sizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          useInheritedMediaQuery: true,
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              primaryColor: Colors.red,
+              appBarTheme: AppBarTheme(
+                color: Colors.red,
+                centerTitle: true,
+              ),
+              scaffoldBackgroundColor: Colors.white,
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurpleAccent,
+                      foregroundColor: Colors.white))),
+          title: 'Ostad Flutter app',
+          home: Moduleninethree(),
+        );
+      }
     );
   }
 }
