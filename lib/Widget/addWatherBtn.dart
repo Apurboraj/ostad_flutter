35666de7a70ae3889117e335607d079fa865c49f
Apurboraj <apucse1296@gmail.com>
2025-03-14 22:18:05 +0600

    updated

---
+++ lib/Widget/addWatherBtn.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class addWatherBtn extends StatelessWidget {
+  final int amount;
+  IconData ? icon;
+  final VoidCallback onClick;
+
+  addWatherBtn({
+   super.key, required this.amount, this.icon, required this.onClick
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton.icon(onPressed: onClick,
+          label: Text("+ ${amount}ml",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.white),),
+          icon: Icon(icon ?? Icons.water_drop,color: Colors.white,),),
+      ),
+    );
+  }
+}
