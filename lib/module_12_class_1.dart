5aecc2b12805135d0662dd99751a89038ed27305
Apurboraj <apucse1296@gmail.com>
2025-03-16 21:24:59 +0600

    updated

--- lib/module_12_class_1.dart
+++ lib/module_12_class_1.dart
@@ -20,23 +20,19 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
 
   double _total = 0.0;
 
-  void _addExpense(String title,double amount, DateTime date,String category){
+  void _addExpense(String title, double amount, DateTime date, String category) {
     setState(() {
       _expense.add(Expense(title: title, amount: amount, date: date, category: category));
       _total += amount;
     });
-
   }
 
-  void _deleteExpense(int index){
+  void _deleteExpense(int index) {
     setState(() {
       _total -= _expense[index].amount;
       _expense.removeAt(index);
     });
   }
-
-
-
 
   void _showForm(BuildContext context) {
     TextEditingController titleController = TextEditingController();
@@ -68,27 +64,23 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
                 ),
                 SizedBox(height: 10,),
                 DropdownButtonFormField<String>(
-                  items: _categories.map((category)=>DropdownMenuItem(value : category , child: Text(category))).toList(),
+                  items: _categories.map((category) => DropdownMenuItem(value: category, child: Text(category))).toList(),
                   onChanged: (value) => selectedCategory = value!,
                   decoration: InputDecoration(labelText: 'Category'),
                 ),
                 SizedBox(height: 20,),
                 SizedBox(
                     width: double.infinity,
-                    child: ElevatedButton(onPressed: (){
-                      if(titleController.text.isEmpty || double.tryParse(amountController.text) == null ){
+                    child: ElevatedButton(onPressed: () {
+                      if (titleController.text.isEmpty || double.tryParse(amountController.text) == null) {
                         return;
                       }
                       _addExpense(titleController.text, double.parse(amountController.text), selectedDate, selectedCategory);
                       titleController.clear();
                       amountController.clear();
                       Navigator.pop(context);
-
-
-
                     }, child: Text("Add Expense"))),
                 SizedBox(height: 20,)
-
               ],
             ),
           );
@@ -124,20 +116,27 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
             child: ListView.builder(
                 itemCount: _expense.length,
                 itemBuilder: (ctx, index) {
-                  return Dismissible(
-                    key: Key(_expense[index].hashCode.toString()),
-                    background: Container(color: Colors.red),
-                    onDismissed: (direction)=>_deleteExpense(index),
-                    child: Card(
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.blueAccent,
-                          child: Text(_expense[index].category),
-                        ),
-                        title: Text(_expense[index].title),
-                        subtitle: Text(
-                            DateFormat.yMMMd().format(_expense[index].date)),
-                        trailing: Text(_expense[index].amount.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                  return Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.blueAccent,
+                        child: Text(_expense[index].category),
+                      ),
+                      title: Text(_expense[index].title),
+                      subtitle: Text(
+                          DateFormat.yMMMd().format(_expense[index].date)),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            _expense[index].amount.toString(),
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete, color: Colors.red),
+                            onPressed: () => _deleteExpense(index),
+                          ),
+                        ],
                       ),
                     ),
                   );
