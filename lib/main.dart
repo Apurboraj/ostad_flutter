57990fe43198550601902086d305dd4a09de6620
Apurboraj <apucse1296@gmail.com>
2026-05-22 17:21:19 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-
-import 'myApp.dart';
-
+import 'package:hello_world/home_screen.dart';
 void main() {
-  runApp(myApp());
+  runApp(const GoogleMapsApp());
+}
+
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
 }
